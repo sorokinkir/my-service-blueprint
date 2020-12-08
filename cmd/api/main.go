3951e78e991922f00ce71391a83f11086eaf5afa
Sorokin Kirill <itari@yandex.ru>
2020-12-08 16:46:51 +0700

    Read cfg v0.1

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/sorokinkir/my-service-blueprint/config"
+)
+
+func main() {
+	cfgPath, err := os.Open("config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err := config.NewConfig(cfgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- config/config.go
+++ config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Config struct for api rest service
+type Config struct {
+	Host string `yaml:"host"`
+	Port string `yaml:"port"`
+}
+
+// NewConfig returns a new decoded Config struct
+func NewConfig(configPath string) (*Config, error) {
+	// create config structure
+	config := &Config{}
+
+	// open config file
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Init new yaml decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
